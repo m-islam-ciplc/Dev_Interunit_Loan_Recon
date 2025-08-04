7075ce521bf4e2390d944d28af701ac75d837ee2
m-islam-ciplc <m.islam@cg-bd.com>
2025-08-04 15:07:34 +0600

    Update header styles
     Header size <h5>

--- static/app.js
+++ static/app.js
@@ -763,7 +763,7 @@ function displayMatches(matches, targetDivId = 'reconciliation-result') {
     let tableHTML = `
         <div class="matched-transactions-wrapper">
             <div class="matched-header">
-                <h4><i class="bi bi-link-45deg"></i> Matched Transactions (${matches.length} pairs)</h4>
+                <h5><i class="bi bi-link-45deg"></i> Matched Transactions (${matches.length} pairs)</h5>
             </div>
             <div class="table-responsive">
                 <table class="matched-transactions-table">
@@ -1420,7 +1420,7 @@ function displayUnmatchedResults(unmatched) {
     let tableHTML = `
         <div class="unmatched-transactions-wrapper">
             <div class="unmatched-header">
-                <h4><i class="bi bi-link-45deg"></i> Unmatched Transactions (${unmatched.length} records)</h4>
+                <h5><i class="bi bi-link-45deg"></i> Unmatched Transactions (${unmatched.length} records)</h5>
             </div>
             <div class="table-responsive">
                 <table class="unmatched-transactions-table">
